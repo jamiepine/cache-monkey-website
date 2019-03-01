3f1d3f7bdd607c4bd821ee34b75fe9d903555abf
jamespine <32987599+jamespine@users.noreply.github.com>
2019-03-01 02:12:53 +0000

    x

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faMagic, faCog } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faLinkedIn,
+  faTwitch
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add({
+  faTwitter,
+  faTwitch,
+  faEnvelope,
+  faMagic,
+  faCog
+});
+
+Vue.component("icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     }
   ]
-})
+});
